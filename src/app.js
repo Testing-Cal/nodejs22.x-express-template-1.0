63ce79f194018f926d47bc080ff89a0bc6c4d452
Yuvraj Dhakate <ydhakate@calibo.com>
2024-11-26 12:48:43 +0530

    code revert

--- src/app.js
+++ src/app.js
@@ -12,21 +12,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Node.js 22 with Express' });
 });
 
-// Dummy API returning JSON
-router.get('/api', function (req, res) {
-  const responseData = {
-    message: 'This is a API response for Get api from Lazsa',
-    success: true,
-    data: {
-      id: 1,
-      name: 'Lazsa Admin',
-      age: 30,
-      profession: 'Admin',
-    },
-  };
-  res.json(responseData);
-});
-
 app.use(process.env.context || "/", router);
 const port = process.env.port || 4000;
 httpServer.listen(port);
